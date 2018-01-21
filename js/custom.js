5a9714729f5b1ab628d7a922fde601b12a1d2d39
VaseRajakrishna <vaseraja888@gmail.com>
2018-01-21 12:07:00 +0530

    go down function

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
- headerPostion();
+$(document).ready(function () {
+    headerPostion();
+    $(".go-down-btn").click(goDown);
 });
-$(window).resize(function(){
-headerPostion();
+$(window).resize(function () {
+    headerPostion();
 });
 
-function headerPostion(){
-    var Height=$(window).height();
-    var Top = (Height- $(".header-content").height() + $(".logo-container").height())/6;
-    var BtmContentTop = (($(".header-content").height() + $(".logo-container").height())+$(".header-btm-content").height())-$(".header-btn-container").height();
-    $(".head-container").css({"height":Height+"px"});
-    $(".header-content").css({"top":Top+"px"});
+function headerPostion() {
+    var Height = $(window).height();
+    var Top = (Height - $(".header-content").height() + $(".logo-container").height()) / 6;
+    var BtmContentTop = (($(".header-content").height() + $(".logo-container").height()) + $(".header-btm-content").height()) - $(".header-btn-container").height();
+    $(".head-container").css({ "height": Height + "px" });
+    $(".header-content").css({ "top": Top + "px" });
     //$(".header-btm-content").css({"top":BtmContentTop+"px"});
+}
+
+function goDown() {
+    var position = $(".r1-container").offset().top;
+    $("HTML, BODY").animate({ scrollTop: position }, 1000);
 }
